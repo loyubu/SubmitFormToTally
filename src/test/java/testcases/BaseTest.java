040d313ed1fafe2ac04dd10c24d984093d14a208
loyubu <admin@transsahara.com>
2026-08-11 16:07:11 +0100

    Commit changes

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -2,7 +2,6 @@ package testcases;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageobjects.FormPage;
